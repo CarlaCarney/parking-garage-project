0482a57dbc4beb186db1bc87cc07132a0e7fdcd6
Diante Dowdell <diante.dowdell99@gmail.com>
2022-10-01 15:12:24 -0500

    added leaveGarage and started runParkingGarage

--- parkingGarage.py
+++ parkingGarage.py
@@ -5,9 +5,6 @@ class ParkingGarage():
         self.parkingSpaces = 9
         self.currentTicket = {}
 
-    def runProgram(self):
-        pass
-
     def takeTicket(self):
         if self.tickets > 0:
             print(f"\nWe currently have {self.tickets} spaces available! \nPrinting Ticket...")
@@ -22,18 +19,42 @@ class ParkingGarage():
             paid = 'Paid'
             print('Your ticket has been paid and you have 15 minutes to leave!')
             self.currentTicket[paid] = True
-            print(self.currentTicket)
         else:
             print("Sorry! Invalid Input, please enter 15!")
             paid = self.payForParking()
 
     def leaveGarage(self):
-        
+        if self.currentTicket.values():
+            print('Thank you, have a nice day!')
+            self.tickets += 1
+            self.parkingSpaces += 1
+        else:
+            print('Please pay for your ticket!')
+            paid = self.payForParking()
+
+# ----- Main Program ---------- #
+
+    def runParkingGarage(self):
+        while True:
+            print("\nWelcome to the parking garage!")
+            response = input('Would you like to purchase a ticket? (Y/N): ')
+            if response.lower() == 'y':
+                self.takeTicket()
+            if response.lower() == 'n':
+                print("Bye! Have a great day!")
+                break
+            else:
+                print("Invalid response. Please type either 'Y' or 'N' ")
+                self.runParkingGarage()
+                response2 = input("Ready to Leave? (Y/N): ")
+                if response2.lower() == 'y':
+                    self.payForParking()
 
 
-
+            
 
 diante = ParkingGarage()
 
-diante.payForParking()
+diante.takeTicket()
+
 
