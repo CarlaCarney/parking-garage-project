5d58df66ad6292d2e01042388f3c6c150cc3c2d6
Diante Dowdell <diante.dowdell99@gmail.com>
2022-10-01 14:50:21 -0500

    added payForParking

--- parkingGarage.py
+++ parkingGarage.py
@@ -5,6 +5,9 @@ class ParkingGarage():
         self.parkingSpaces = 9
         self.currentTicket = {}
 
+    def runProgram(self):
+        pass
+
     def takeTicket(self):
         if self.tickets > 0:
             print(f"\nWe currently have {self.tickets} spaces available! \nPrinting Ticket...")
@@ -13,8 +16,24 @@ class ParkingGarage():
         else:
             print("Sorry we have no parking spaces available at the moment!")
 
+    def payForParking(self):
+        paid = input("Please enter 15, to pay for ticket: ")
+        if paid == '15':
+            paid = 'Paid'
+            print('Your ticket has been paid and you have 15 minutes to leave!')
+            self.currentTicket[paid] = True
+            print(self.currentTicket)
+        else:
+            print("Sorry! Invalid Input, please enter 15!")
+            paid = self.payForParking()
+
+    def leaveGarage(self):
+        
+
+
+
 
 diante = ParkingGarage()
 
-diante.takeTicket()
+diante.payForParking()
 
